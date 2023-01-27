2f49b12aa07259eba43b918d00c1ada2d0e8497d
Eduardo Castro <eduardofc.dev@gmail.com>
2023-01-27 10:47:05 -0300

    Bugfix para URL de API

--- src/app/services/produto.service.ts
+++ src/app/services/produto.service.ts
@@ -12,27 +12,27 @@ export class ProdutoService {
   constructor(private http : HttpClient) { }
 
   public findAll(): Observable<Produto[]> {
-    return this.http.get<Produto[]>(`${API_CONFIG}/produto/lista`);
+    return this.http.get<Produto[]>(`${API_CONFIG.baseUrl}/produto/lista`);
   }
 
   public findById(id: number): Observable<Produto> {
-    return this.http.get<Produto>(`${API_CONFIG}/produto/ler/${id}`);
+    return this.http.get<Produto>(`${API_CONFIG.baseUrl}/produto/ler/${id}`);
   }
 
   public findByNome(nome: string): Observable<Produto> {
-    return this.http.get<Produto>(`${API_CONFIG}/produto/lerNome/${nome}`);
+    return this.http.get<Produto>(`${API_CONFIG.baseUrl}/produto/lerNome/${nome}`);
   }
 
   public create(produto: Produto): Observable<any> {
-    return this.http.post<Produto>(`${API_CONFIG}/produto/criar` , produto);
+    return this.http.post<Produto>(`${API_CONFIG.baseUrl}/produto/criar` , produto);
   }
 
   public update(id: number, produto: Produto): Observable<any> {
-    return this.http.put<any>(`${API_CONFIG}/produto/atualizar/${id}` , produto);
+    return this.http.put<any>(`${API_CONFIG.baseUrl}/produto/atualizar/${id}` , produto);
   }
 
   public delete(id: number): Observable<any> {
-    return this.http.delete<any>(`${API_CONFIG}/produto/deletar/${id}`);
+    return this.http.delete<any>(`${API_CONFIG.baseUrl}/produto/deletar/${id}`);
   }
 
 }
