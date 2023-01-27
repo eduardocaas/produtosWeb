b58defbb2f2e4b23bcc169910d7d24ef25e98540
Eduardo Castro <eduardofc.dev@gmail.com>
2023-01-27 10:46:50 -0300

    HTML e CSS para componente app

---
+++ src/app/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const $navLinks = Array.prototype.slice.call(document.querySelectorAll('.nav-link'), 0);
+
+  if ($navLinks.length > 0) {
+    $navLinks.forEach( el => {
+      if(window.location.pathname == el.getAttribute("href")){
+         el.className += " current";
+      }
+    })
+  }
+});
