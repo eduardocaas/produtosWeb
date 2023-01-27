a6349955c5087eaaa34c30de5ba9229468dc16ea
Eduardo Castro <eduardofc.dev@gmail.com>
2023-01-27 10:46:07 -0300

    Rotas para produto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditarProdutoComponent } from './components/produto/editar-produto/editar-produto.component';
+import { LerProdutoComponent } from './components/produto/ler-produto/ler-produto.component';
+import { ListaProdutoComponent } from './components/produto/lista-produto/lista-produto.component';
+import { NovoProdutoComponent } from './components/produto/novo-produto/novo-produto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ListaProdutoComponent},
+  {path: 'ler/:id', component: LerProdutoComponent},
+  {path: 'novo', component: NovoProdutoComponent},
+  {path: 'editar/:id', component: EditarProdutoComponent},
+  {path: '**', redirectTo: '', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
